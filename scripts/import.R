7b94baa8179e0441cbf89e9413ee3b7170efc507
barbaczek <barbaczek123@gmail.com>
2024-07-03 18:19:55 +0200

    Update import.R

--- scripts/import.R
+++ scripts/import.R
@@ -1,1 +1,11 @@
+#!/usr/bin/env Rscript
 
+library(tidyverse)
+library(tidymodels)
+
+args   = commandArgs(trailingOnly=TRUE)
+input  = args[1]
+
+dataset = read_tsv(input)
+
+saveRDS(dataset, file = "input_dataset.rds")
